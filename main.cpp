211078574911afb23087128af29369d69d9df06d
jiachun23 <52855256+jiachun23@users.noreply.github.com>
2019-09-16 17:02:46 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+class Train {
+
+
+void getTrainID()
+
+
+void getTrainDeparture()
+
+
+void getTrainDestination()
+
+
+void getTrainTicket_Fee()
+
+
+void getTrainSeating()
+
+
+void getClassType()
+
+
+void getTrainDeparture_Time()
+
+
+void getTrainArrival_Time()
+
+
+void getTrainMeal()
+
+
+};
+
+
+class Passenger {
+
+
+
+void getName()
+
+
+
+void getPhoneNum()
+
+
+void getPassengerIC()
+
+
+
+void getAmount_Ticket()
+
+
+
+void getBookingID()
+
+
+void showBooking()
+
+
+
+void deleteBooking()
+
+
+void checkBan_List()
+
+
+void BanPassenger()
+
+
+double Total_BookingFee()
+
+
+};
+
+
+
+class Calender {
+
+void getDay()
+
+
+void getMonth()
+
+
+void getYear()
+
+};
+
+int main()
+{
+
+}
